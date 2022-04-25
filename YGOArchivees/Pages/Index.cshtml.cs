2a4d4afa5786a339b784b76cb951a86085800338
mark burya <mark.burya@studentcentralia.onmicrosoft.com>
2022-04-24 17:34:37 -0700

    cahnges v0.1

--- YGOArchivees/Pages/Index.cshtml.cs
+++ YGOArchivees/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace YGOArchivees.Pages
+namespace YGOArchives.Pages
 {
     public class IndexModel : PageModel
     {
