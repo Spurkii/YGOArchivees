7272bb04c7e173b3b32b01b7603b04a5e042762f
mark burya <mark.burya@studentcentralia.onmicrosoft.com>
2022-05-01 16:09:02 -0700

    search bar + step closer to api working

--- YGOArchivees/wwwroot/js/site.js
+++ YGOArchivees/wwwroot/js/site.js
@@ -1,4 +1,27 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+// for details on configuring this project to bundle and minify static web assets.0
 
-// Write your JavaScript code.
+$.getJSON(
+    "https://db.ygoprodeck.com/api/v7/cardinfo.php",
+    function (data) {
+        console.log(data);
+        var id = data.id;
+        var name = data.name;
+        var type = data.type;
+        var race = data.race;
+        var desc = data.desc;
+        var atk = data.atk;
+        var def = data.def;
+        var level = data.level;
+        var card_sets = data.card_sets;
+
+        $(".id").append(id);
+        $(".name").append(name);
+        $(".type").append(type);
+        $(".race").append(race);
+        $(".desc").append(desc);
+        $(".atk").append(atk);
+        $(".def").append(def);
+        $(".level").append(level);
+        $(".card_sets").append(card_sets);
+    });
